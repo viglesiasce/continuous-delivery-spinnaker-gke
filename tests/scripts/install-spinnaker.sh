8d4b541e382411f6a432bbaad506a9bc4cd60cf8
Vic Iglesias <viglesias@google.com>
2018-02-27 13:10:15 -0800

    Add spinnaker's SA RBAC command

--- tests/scripts/install-spinnaker.sh
+++ tests/scripts/install-spinnaker.sh
@@ -20,6 +20,7 @@ cp linux-amd64/helm .
 kubectl create clusterrolebinding user-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value account)
 kubectl create serviceaccount tiller --namespace kube-system
 kubectl create clusterrolebinding tiller-admin-binding --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl create clusterrolebinding --clusterrole=cluster-admin --serviceaccount=default:default spinnaker-admin
 
 # Initialize Helm
 ./helm init --service-account tiller
