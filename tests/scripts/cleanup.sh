0f16b1d3d7265bbd240049f41b125aadb21a38a0
Vic Iglesias <viglesias@google.com>
2018-11-08 11:37:33 -0800

    Fix cleanup

--- tests/scripts/cleanup.sh
+++ tests/scripts/cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
-export SA_EMAIL=$(gcloud iam service-accounts list --filter="displayName:spinnaker-storage-account" --format='value(email)')
+export SA_EMAIL=$(gcloud iam service-accounts list --filter="displayName:spinnaker-account" --format='value(email)')
 export PROJECT=$(gcloud info --format='value(config.project)')
 export BUCKET=$PROJECT-spinnaker-config
 
@@ -10,6 +10,6 @@ echo y | gcloud source repos delete sample-app || true
 gsutil -m rm -r gs://$BUCKET || true
 rm -f spinnaker-sa.json
 rm -f spinnaker-config.yaml
-rm -f helm-v2.5.0-linux-amd64.tar.gz
+rm -f helm-v2.10.0-linux-amd64.tar.gz
 rm -rf linux-amd64
 rm -f helm
