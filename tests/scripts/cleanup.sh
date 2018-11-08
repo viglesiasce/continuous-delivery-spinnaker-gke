bdb4c5f78b3f580f27b00f775f127f89cd1820c3
Vic Iglesias <viglesias@google.com>
2018-11-08 14:42:21 -0800

    Cleanup manifest bucket

--- tests/scripts/cleanup.sh
+++ tests/scripts/cleanup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -xe
 export SA_EMAIL=$(gcloud iam service-accounts list --filter="displayName:spinnaker-account" --format='value(email)')
 export PROJECT=$(gcloud info --format='value(config.project)')
-export BUCKET=$PROJECT-spinnaker-config
+export CONFIG_BUCKET=$PROJECT-spinnaker-config
+export MANIFEST_BUCKET=$PROJECT-kubernetes-manifests
 
 gcloud projects remove-iam-policy-binding $PROJECT --role roles/storage.admin --member serviceAccount:$SA_EMAIL || true
 gcloud projects remove-iam-policy-binding $PROJECT --role roles/pubsub.subscriber --member serviceAccount:$SA_EMAIL || true
@@ -9,7 +10,8 @@ echo y | gcloud iam service-accounts delete $SA_EMAIL || true
 echo y | gcloud container clusters delete spinnaker-tutorial --zone us-central1-f || true
 echo y | gcloud source repos delete sample-app || true
 gcloud beta pubsub subscriptions delete gcr-triggers || true
-gsutil -m rm -r gs://$BUCKET || true
+gsutil -m rm -r gs://$CONFIG_BUCKET || true
+gsutil -m rm -r gs://$MANIFEST_BUCKET || true
 rm -f spinnaker-sa.json
 rm -f spinnaker-config.yaml
 rm -f helm-v2.10.0-linux-amd64.tar.gz
