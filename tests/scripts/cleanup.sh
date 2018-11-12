0a7921ad06e428c5d364f72fc508cd1a84bc58b2
Vic Iglesias <viglesias@google.com>
2018-11-12 13:54:16 -0800

    Update for Spinnaker v1 Chart (#4)
    * Update for spinnaker v1 chart
    
    * Update for spinnaker v1 chart
    
    * Undo changes to README.md
    
    * Fix cleanup
    
    * Set pipefail
    
    * Allow topic creation to fail if it already exists
    
    * Remove specific project name from K8s deployments
    
    * Remove unset replicas from deployment
    
    * Add spinnaker piece of tutorial to test
    
    * Delete GCR subscription
    
    * Use iam policy specific to subscription
    
    * Remove pubsub role from service account
    
    * Wait for pods to settle before doing a port-forward
    
    * Cleanup manifest bucket
    
    * Remove correct pubsub iam binding
    
    * add timeout to cleanup and install scripts
    
    * Log port-forward output to console
    
    * Wait for Gate to come up when sending spin commands
    
    * Add back sleep which helped with SA role getting added properly
    
    * Set replicas for all deployments
    
    * terminate port-forward in install test
    
    * Add cleanup at end of install
    
    * Poll for running deck pod before creating port-forward
    
    * Use build ID in iam service account name
    
    * Use date to differentiate SA
    
    * Remove sleep from install script
    
    * Fix service account cleanup

--- tests/scripts/cleanup.sh
+++ tests/scripts/cleanup.sh
@@ -1,15 +1,21 @@
 #!/bin/bash -xe
-export SA_EMAIL=$(gcloud iam service-accounts list --filter="displayName:spinnaker-storage-account" --format='value(email)')
 export PROJECT=$(gcloud info --format='value(config.project)')
-export BUCKET=$PROJECT-spinnaker-config
+export CONFIG_BUCKET=$PROJECT-spinnaker-config
+export MANIFEST_BUCKET=$PROJECT-kubernetes-manifests
 
-gcloud projects remove-iam-policy-binding $PROJECT --role roles/storage.admin --member serviceAccount:$SA_EMAIL || true
-echo y | gcloud iam service-accounts delete $SA_EMAIL || true
+for SA_EMAIL in `gcloud iam service-accounts list --filter="displayName:spinnaker-account" --format='value(email)'`;do
+  gcloud projects remove-iam-policy-binding $PROJECT --role roles/storage.admin --member serviceAccount:$SA_EMAIL || true
+  gcloud beta pubsub subscriptions remove-iam-policy-binding gcr-triggers --role roles/pubsub.subscriber --member serviceAccount:$SA_EMAIL || true
+  echo y | gcloud iam service-accounts delete $SA_EMAIL || true
+done
+
 echo y | gcloud container clusters delete spinnaker-tutorial --zone us-central1-f || true
 echo y | gcloud source repos delete sample-app || true
-gsutil -m rm -r gs://$BUCKET || true
+gcloud beta pubsub subscriptions delete gcr-triggers || true
+gsutil -m rm -r gs://$CONFIG_BUCKET || true
+gsutil -m rm -r gs://$MANIFEST_BUCKET || true
 rm -f spinnaker-sa.json
 rm -f spinnaker-config.yaml
-rm -f helm-v2.5.0-linux-amd64.tar.gz
+rm -f helm-v2.10.0-linux-amd64.tar.gz
 rm -rf linux-amd64
 rm -f helm
--- tests/scripts/install-spinnaker.sh
+++ tests/scripts/install-spinnaker.sh
@@ -1,72 +1,104 @@
 #!/bin/bash -xe
 gcloud config set compute/zone us-central1-f
-gcloud container clusters create spinnaker-tutorial --machine-type=n1-standard-2
-gcloud iam service-accounts create  spinnaker-storage-account  --display-name spinnaker-storage-account
+gcloud container clusters create spinnaker-tutorial \
+        --machine-type=n1-standard-2
+SA_NAME=spinnaker-account-$(date +%s)
+gcloud iam service-accounts create $SA_NAME \
+        --display-name $SA_NAME
 
-# Needed to ensure SA gets role properly?
-
-sleep 60
-export SA_EMAIL=$(gcloud iam service-accounts list --filter="displayName:spinnaker-storage-account" --format='value(email)')
+export SA_EMAIL=$(gcloud iam service-accounts list \
+    --filter="displayName:$SA_NAME" \
+    --format='value(email)')
 export PROJECT=$(gcloud info --format='value(config.project)')
-gcloud projects add-iam-policy-binding $PROJECT --role roles/storage.admin --member serviceAccount:$SA_EMAIL
+gcloud projects add-iam-policy-binding \
+        $PROJECT --role roles/storage.admin --member serviceAccount:$SA_EMAIL
 gcloud iam service-accounts keys create spinnaker-sa.json --iam-account $SA_EMAIL
 
-export HELM_VERSION=2.7.2
-wget https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz
-tar zxfv helm-v${HELM_VERSION}-linux-amd64.tar.gz
-cp linux-amd64/helm .
+# Create PubSub trigger
+gcloud beta pubsub topics create projects/$PROJECT/topics/gcr || true
+gcloud beta pubsub subscriptions create gcr-triggers \
+    --topic projects/${PROJECT}/topics/gcr
+export SA_EMAIL=$(gcloud iam service-accounts list \
+    --filter="displayName:$SA_NAME" \
+    --format='value(email)')
+gcloud beta pubsub subscriptions add-iam-policy-binding gcr-triggers \
+        --role roles/pubsub.subscriber --member serviceAccount:$SA_EMAIL
 
-# Give tiller cluster-admin role service account
+# Deploy Spinnaker using Helm
+wget https://storage.googleapis.com/kubernetes-helm/helm-v2.10.0-linux-amd64.tar.gz
+tar zxfv helm-v2.10.0-linux-amd64.tar.gz
+cp linux-amd64/helm .
 kubectl create clusterrolebinding user-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value account)
 kubectl create serviceaccount tiller --namespace kube-system
 kubectl create clusterrolebinding tiller-admin-binding --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+
 kubectl create clusterrolebinding --clusterrole=cluster-admin --serviceaccount=default:default spinnaker-admin
 
-# Initialize Helm
-./helm init --service-account tiller
+./helm init --service-account=tiller
 ./helm update
-# Give tiller a chance to start up
-# TODO: Change this to polling
-sleep 180
-./helm version | grep ${HELM_VERSION}
 
-export PROJECT=$(gcloud info --format='value(config.project)')
+until ./helm version; do sleep 10;done
+
+export PROJECT=$(gcloud info \
+    --format='value(config.project)')
 export BUCKET=$PROJECT-spinnaker-config
 gsutil mb -c regional -l us-central1 gs://$BUCKET
 
-export SA_JSON=`cat spinnaker-sa.json`
+export SA_JSON=$(cat spinnaker-sa.json)
 export PROJECT=$(gcloud info --format='value(config.project)')
 export BUCKET=$PROJECT-spinnaker-config
 cat > spinnaker-config.yaml <<EOF
-storageBucket: $BUCKET
 gcs:
   enabled: true
+  bucket: $BUCKET
   project: $PROJECT
   jsonKey: '$SA_JSON'
 
-# Disable minio the default
-minio:
-  enabled: false
-
-# Configure your Docker registries here
-accounts:
+dockerRegistries:
 - name: gcr
   address: https://gcr.io
-  repositories:
-  - $PROJECT/sample-app
   username: _json_key
   password: '$SA_JSON'
   email: 1234@5678.com
-images:
-  deck: quay.io/spinnaker/deck:v2.1095.0
+
+
+# Disable minio as the default storage backend
+minio:
+  enabled: false
+
+
+# Configure Spinnaker to enable GCP services
+halyard:
+  spinnakerVersion: 1.10.2
+  image:
+    tag: 1.12.0
+  additionalScripts:
+    create: true
+    data:
+      enable_gcs_artifacts.sh: |-
+        \$HAL_COMMAND config artifact gcs account add gcs-$PROJECT --json-path /opt/gcs/key.json
+        \$HAL_COMMAND config artifact gcs enable
+      enable_pubsub_triggers.sh: |-
+        \$HAL_COMMAND config pubsub google enable
+        \$HAL_COMMAND config pubsub google subscription add gcr-triggers \
+          --subscription-name gcr-triggers \
+          --json-path /opt/gcs/key.json \
+          --project $PROJECT \
+          --message-format GCR
 EOF
 
-./helm install -n cd stable/spinnaker -f spinnaker-config.yaml --version 0.3.1 --timeout 600
+./helm install -n cd stable/spinnaker -f spinnaker-config.yaml --timeout 600 \
+    --version 1.1.6 --wait
 
-export DECK_POD=$(kubectl get pods --namespace default -l "component=deck" -o jsonpath="{.items[0].metadata.name}")
-kubectl port-forward --namespace default $DECK_POD 8080:9000 >> /dev/null &
-sleep 10
-curl localhost:8080
+export DECK_POD=$(kubectl get pods --namespace default -l "cluster=spin-deck" \
+    -o jsonpath="{.items[0].metadata.name}")
+
+# Wiat for pods to settle before doing port-forward
+until kubectl get pods $DECK_POD | grep Running; do sleep 10;done
+kubectl port-forward --namespace default $DECK_POD 8080:9000 &
+
+
+until curl localhost:8080; do sleep 10;done
 
 rm -rf .git
 git config --global user.email "person@example.org"
@@ -79,5 +111,30 @@ git config credential.helper gcloud.sh
 export PROJECT=$(gcloud info --format='value(config.project)')
 git remote add origin https://source.developers.google.com/p/$PROJECT/r/sample-app
 git push origin master
+
+# Setup K8s manifests
+export PROJECT=$(gcloud info --format='value(config.project)')
+gsutil mb -l us-central1 gs://$PROJECT-kubernetes-manifests
+gsutil versioning set on gs://$PROJECT-kubernetes-manifests
+sed -i s/PROJECT/$PROJECT/g k8s/deployments/*
+
+# Install spin CLI
+curl -LO https://storage.googleapis.com/spinnaker-artifacts/spin/$(curl -s https://storage.googleapis.com/spinnaker-artifacts/spin/latest)/linux/amd64/spin
+chmod +x spin
+
+# Create application
+until timeout 1m ./spin application save --application-name sample \
+                        --owner-email example@example.com \
+                        --cloud-providers kubernetes \
+                        --gate-endpoint http://localhost:8080/gate; do sleep 10;done
+
+# Create pipeline
+export PROJECT=$(gcloud info --format='value(config.project)')
+sed s/PROJECT/$PROJECT/g spinnaker/pipeline-deploy.json > pipeline.json
+./spin pipeline save --gate-endpoint http://localhost:8080/gate -f pipeline.json
+
+# Push a tag
+git tag v1.0.0
+git push --tags
 
 kill %1
